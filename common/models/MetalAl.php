a79e905592570eee70e4285065e5fbde2366f6e6
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-08-11 18:22:26 +0800

    Created metal-al model and search

---
+++ common/models/MetalAl.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "metal_al".
+ *
+ * @property integer $id
+ * @property integer $import_metal_id
+ * @property string $date_uploaded
+ * @property string $date
+ * @property string $al_cash
+ * @property string $al_three_month
+ * @property string $al_stocl
+ */
+class MetalAl extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'metal_al';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['import_metal_id', 'date_uploaded', 'date', 'al_cash', 'al_three_month', 'al_stocl'], 'required'],
+            [['import_metal_id'], 'integer'],
+            [['date_uploaded'], 'safe'],
+            [['al_cash', 'al_three_month', 'al_stocl'], 'number'],
+            [['date'], 'string', 'max' => 25],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'import_metal_id' => 'Import Metal ID',
+            'date_uploaded' => 'Date Uploaded',
+            'date' => 'Date',
+            'al_cash' => 'Al Cash',
+            'al_three_month' => 'Al Three Month',
+            'al_stocl' => 'Al Stocl',
+        ];
+    }
+}
---
+++ common/models/MetalAlSearch.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\MetalAl;
+
+/**
+ * MetalAlSearch represents the model behind the search form about `common\models\MetalAl`.
+ */
+class MetalAlSearch extends MetalAl
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'import_metal_id'], 'integer'],
+            [['date_uploaded', 'date'], 'safe'],
+            [['al_cash', 'al_three_month', 'al_stocl'], 'number'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = MetalAl::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'import_metal_id' => $this->import_metal_id,
+            'date_uploaded' => $this->date_uploaded,
+            'al_cash' => $this->al_cash,
+            'al_three_month' => $this->al_three_month,
+            'al_stocl' => $this->al_stocl,
+        ]);
+
+        $query->andFilterWhere(['like', 'date', $this->date]);
+
+        return $dataProvider;
+    }
+}
