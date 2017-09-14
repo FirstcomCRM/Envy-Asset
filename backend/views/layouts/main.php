36d7dfd894897ab23f4f2039ef23974122681eba
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-09-14 17:28:03 +0800

    updated nav bar

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -71,7 +71,7 @@ AppAsset::register($this);
           //  ['label'=>'User Log Files', 'url'=>['/user-log/index']],
             ['label'=>'Investment Overview', 'url'=>['toollist/index']],
             ['label'=>'Commission Report', 'url'=>['commission/index']],
-            ['label'=>'Customer Report', 'url'=>['toollist/index']],
+            ['label'=>'Investor Report', 'url'=>['investor-report/index']],
         ],
       ],
         ['label'=>'Setup', 'class'=>'navbars', 'items'=>[
