74147f314e4501b060f6114e10265eed4fec9f40
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-09-15 17:54:32 +0800

    changed id selector to class selector

--- backend/web/js/modals.js
+++ backend/web/js/modals.js
@@ -1,10 +1,11 @@
 $(function(){
-  $('#modalButton').click(function(){ //#modalButton, changed to class when in gridview customer link
-    $('#modal').modal('show')
+//  $('#modalButton').click(function(){ //#modalButton, changed to class when in gridview customer link
+  $('.modalButton').click(function(e){
+    e.preventDefault();
+    console.log('test');
+    $('#modals').modal('show')
       .find('#modalContent')
       .load($(this).attr('value'));
   });
-
-  
 
 });
