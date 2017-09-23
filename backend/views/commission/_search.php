66260bc2ecd69f82471951eb25c1e895d95cfc44
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-09-23 15:38:41 +0800

    added salesperson search filter

--- backend/views/commission/_search.php
+++ backend/views/commission/_search.php
@@ -2,10 +2,16 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
 use kartik\daterange\DateRangePicker;
+use kartik\widgets\Select2;
+use common\models\UserManagement;
 /* @var $this yii\web\View */
 /* @var $model common\models\CommissionSearch */
 /* @var $form yii\widgets\ActiveForm */
+
+$data = UserManagement::find()->select(['id','name'])->orderBy(['name'=>SORT_ASC])->where(['apply_tier'=>1])->all();
+$sales = ArrayHelper::map($data,'id','name');
 ?>
 
 <div class="commission-search">
@@ -15,27 +21,44 @@ use kartik\daterange\DateRangePicker;
         'method' => 'get',
     ]); ?>
 
-    <?php $form->field($model, 'transact_date') ?>
-
-    <?php echo $form->field($model, 'transact_date')->label()->widget(DateRangePicker::classname(), [
-      'useWithAddon'=>false,
-      'convertFormat'=>true,
-      'pluginOptions'=>[
-        'locale'=>[
-          //'format'=> 'M j Y',
-          //'format'=> 'm-d-Y',
-          'format'=> 'Y-m-d',
-        ],
-      ],
-      'options'=>[
-        'placeholder'=>'Select Date Range',
-        'class'=>'form-control'
-      ],
-    ]);?>
-    <div class="form-group">
-        <?= Html::submitButton('<i class="fa fa-search" aria-hidden="true"></i> Search', ['class' => 'btn btn-default']) ?>
-        <?php echo Html::a('<i class="fa fa-undo" aria-hidden="true"></i> Reset',['index'],['class' => 'btn btn-default']) ?>
+    <div class="row">
+      <div class="col-md-3">
+        <?php echo $form->field($model, 'transact_date')->label(false)->widget(DateRangePicker::classname(), [
+          'useWithAddon'=>false,
+          'convertFormat'=>true,
+          'pluginOptions'=>[
+            'locale'=>[
+              //'format'=> 'M j Y',
+              //'format'=> 'm-d-Y',
+              'format'=> 'Y-m-d',
+            ],
+          ],
+          'options'=>[
+            'placeholder'=>'Transaction Date Range',
+            'class'=>'form-control'
+          ],
+        ]);?>
+      </div>
+      <div class="col-md-3">
+        <?php echo $form->field($model,'sales_person')->label(false)->widget(Select2::className(),[
+            'data'=>$sales,
+            'options'=>['placeholder'=>'Sales Person '],
+            'theme'=> Select2::THEME_BOOTSTRAP,
+            'size'=> Select2::MEDIUM,
+            'pluginOptions' => [
+              'allowClear' => true
+            ],
+          ]) ?>
+      </div>
+      <div class="col-md-3">
+        <div class="form-group">
+            <?= Html::submitButton('<i class="fa fa-search" aria-hidden="true"></i> Search', ['class' => 'btn btn-default']) ?>
+            <?php echo Html::a('<i class="fa fa-undo" aria-hidden="true"></i> Reset',['index'],['class' => 'btn btn-default']) ?>
+        </div>
+      </div>
     </div>
+
+
 
     <?php ActiveForm::end(); ?>
 
