4539b1e64bbe336ca3bf3daeb0336f105c6fb179
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-10-06 16:50:52 +0800

    added new filter options and display gridview

--- backend/views/investor-report/_search.php
+++ backend/views/investor-report/_search.php
@@ -7,10 +7,12 @@ use kartik\widgets\Select2;
 use kartik\daterange\DateRangePicker;
 use common\models\Investor;
 use common\models\ProductManagement;
+use common\models\UserManagement;
 /* @var $this yii\web\View */
 /* @var $model common\models\PurchaseSearch */
 /* @var $form yii\widgets\ActiveForm */
-
+$data = UserManagement::find()->select(['id','name'])->orderBy(['name'=>SORT_ASC])->where(['apply_tier'=>1])->all();
+$sales = ArrayHelper::map($data,'id','name');
 $data = null;
 ?>
 
@@ -21,28 +23,42 @@ $data = null;
         'method' => 'get',
     ]); ?>
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-3">
         <?php echo $form->field($model, 'date')->label(false)->widget(DateRangePicker::classname(), [
-        'useWithAddon'=>false,
-        'convertFormat'=>true,
-        'pluginOptions'=>[
-          'locale'=>[
-            //'format'=> 'M j Y',
-            //'format'=> 'm-d-Y',
-            'format'=> 'Y-m-d',
-          ],
-        ],
-        'options'=>[
-          'placeholder'=>'Select Date Range',
-          'class'=>'form-control'
-        ],
-    ]);?>
+            'useWithAddon'=>false,
+            'convertFormat'=>true,
+            'pluginOptions'=>[
+              'locale'=>[
+                //'format'=> 'M j Y',
+                //'format'=> 'm-d-Y',
+                'format'=> 'Y-m-d',
+              ],
+            ],
+            'options'=>[
+              'placeholder'=>'Select Date Range',
+              'class'=>'form-control'
+            ],
+          ]);?>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-3">
+        <?php echo $form->field($model,'salesperson')->label(false)->widget(Select2::className(),[
+            'data'=>$sales,
+            'options'=>['placeholder'=>'Sales Person '],
+            'theme'=> Select2::THEME_BOOTSTRAP,
+            'size'=> Select2::MEDIUM,
+            'pluginOptions' => [
+              'allowClear' => true
+            ],
+          ]) ?>
+      </div>
+      <div class="col-md-3">
+        <?php echo $form->field($model,'id')->textInput(['placeholder'=>'Purchase No'])->label(false) ?>
+      </div>
+      <div class="col-md-3">
           <div class="form-group">
               <?= Html::submitButton('<i class="fa fa-search" aria-hidden="true"></i> Search', ['class' => 'btn btn-default']) ?>
               <?php echo Html::a('<i class="fa fa-undo" aria-hidden="true"></i> Reset',['index'],['class' => 'btn btn-default']) ?>
-            
+
           </div>
       </div>
     </div>
--- backend/views/investor-report/index.php
+++ backend/views/investor-report/index.php
@@ -44,7 +44,7 @@ $this->params['breadcrumbs'][] = $this->title;
           //  'filterModel' => $searchModel,
             'columns' => [
                 ['class' => 'yii\grid\SerialColumn'],
-            //    'id',
+                'id',
                 [
                   'attribute'=>'investor',
                   'format'=>'raw',
