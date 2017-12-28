aaa6e87d4e6de5be42f5fb74c55cc982940f4b8b
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-12-28 18:05:24 +0800

    added common local parms

---
+++ common/config/params-local.php
@@ -0,0 +1,8 @@
+<?php
+
+yii::setAlias('@metal', 'uploads/metal_stock');
+yii::setAlias('@images', 'images');
+yii::setAlias('@investor','uploads/investor');
+yii::setAlias('@template','template');
+return [
+];
