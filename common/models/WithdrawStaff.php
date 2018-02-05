d1ec40e3227ccf36504a7ec656d3cbaf2c299e0b
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-02-05 17:37:56 +0800

    model for withdrawStaff module

---
+++ common/models/WithdrawStaff.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "withdraw_staff".
+ *
+ * @property integer $id
+ * @property integer $staff
+ * @property string $price
+ * @property string $category
+ * @property string $date
+ * @property string $remarks
+ * @property string $date_added
+ */
+class WithdrawStaff extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'withdraw_staff';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['staff', 'price', 'category', 'date'], 'required'],
+            [['staff'], 'integer'],
+            [['price'], 'number'],
+            [['date', 'date_added'], 'safe'],
+            [['remarks'], 'string'],
+            [['category'], 'string', 'max' => 75],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'staff' => 'Staff',
+            'price' => 'Amount',
+            'category' => 'Product Category',
+            'date' => 'Date',
+            'remarks' => 'Remarks',
+            'date_added' => 'Date Added',
+        ];
+    }
+}
