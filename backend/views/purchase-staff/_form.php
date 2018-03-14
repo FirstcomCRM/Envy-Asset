a0a4c54919f97fd1e85a41625e05d447dc0bec1b
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-03-14 13:41:00 +0800

    added 3 new earning field

--- backend/views/purchase-staff/_form.php
+++ backend/views/purchase-staff/_form.php
@@ -21,6 +21,17 @@ $prod = ArrayHelper::map($data,'id','product_name');
 $data = UserManagement::find()->orderBy(['name'=>SORT_ASC])->select(['id','name'])->where(['apply_tier'=>1])->all();
 $users = ArrayHelper::map($data,'id','name');
 
+$pur_type = [
+  'Metal'=>'Metal',
+  'Nickel'=>'Nickel',
+];
+
+$ch_type = [
+  'Tier'=>'Tier',
+  'Others'=>'Others',
+];
+
+
 $data = null;
 
 /*$salesperson = Usermanagement::find()->where(['id'=>$model->salesperson])->one();
@@ -63,13 +74,11 @@ if (empty($salesperson)) {
             ],
           ]) ?>
 
-        <?= $form->field($model, 'share')->textInput(['maxlength' => true]) ?>
-        <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
-      </div>
-      <div class="col-md-6">
+        <?= $form->field($model, 'share')->textInput(['maxlength' => true ]) ?>
+        <?= $form->field($model, 'price')->textInput(['maxlength' => true,'id'=>'s_price']) ?>
 
         <?php echo $form->field($model, 'date')->widget(DatePicker::classname(), [
-          //'options' => ['placeholder' => 'Date'],
+        'options' => ['id' => 's_purchase_date'],
         //  'value' => '08/10/2004',
           'convertFormat'=>true,
           'readonly' => true,
@@ -79,16 +88,44 @@ if (empty($salesperson)) {
             'format' => 'php:d M Y',
           ]
         ]); ?>
-        <?= $form->field($model, 'remarks')->textarea(['rows' => 6]) ?>
+        <?php echo $form->field($model, 'expiry_date')->widget(DatePicker::classname(), [
+          //'options' => ['placeholder' => 'Date'],
+            'options' => ['id' => 's_expiry_date'],
+        //  'value' => '08/10/2004',
+          'convertFormat'=>true,
+          'readonly' => true,
+          'pluginOptions' => [
+            'autoclose'=>true,
+          //  'format' => 'mm/dd/yyyy'
+            'format' => 'php:d M Y',
+          ]
+        ]); ?>
+
+      </div>
+      <div class="col-md-6">
+
+
+
+        <?= $form->field($model, 'remarks')->textarea(['rows' => 4]) ?>
+        <?= $form->field($model, 'sum_all')->textInput(['id'=>'s_sum_all','readOnly'=>true]) ?>
+        <?php echo $form->field($model, 'trading_days') ?>
+        <?php echo $form->field($model, 'prorated_days') ?>
+        <?php echo $form->field($model, 'purchase_type')->radioList($pur_type,['id'=>'purchase_type', 'class'=>'purchase_type']); ?>
+
+
+        <?php echo  $form->field($model, 'charge_type')->dropDownList($ch_type,['id'=>'s_charge_type'])?>
+        <?php echo $form->field($model, 'company_charge')->textInput(['id'=>'s_company_charge']) ?>
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? '<i class="fa fa-pencil" aria-hidden="true"></i> Create' : '<i class="fa fa-pencil-square-o" aria-hidden="true"></i> Update',
+            ['class' => $model->isNewRecord ? 'btn btn-default' : 'btn btn-default']) ?>
+        </div>
+
       </div>
     </div>
 
 
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? '<i class="fa fa-pencil" aria-hidden="true"></i> Create' : '<i class="fa fa-pencil-square-o" aria-hidden="true"></i> Update',
-        ['class' => $model->isNewRecord ? 'btn btn-default' : 'btn btn-default']) ?>
-    </div>
+
 
     <?php ActiveForm::end(); ?>
 
