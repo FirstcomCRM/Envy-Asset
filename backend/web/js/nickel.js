cb3ba62cc7fa8e824c1855d9dbef0a4bfa791039
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-03-20 17:54:31 +0800

    used ajax functionality to auto-calculate the required information

--- backend/web/js/nickel.js
+++ backend/web/js/nickel.js
@@ -1,77 +1,63 @@
 $(document).ready(function(){
 
+
+  $('#metalnickeldeals-purchase_price_a').on('change',function(){
+    sums();
+  });
+
+  $('#metalnickeldeals-insurance_cost_a').on('change',function(){
+    sums();
+  });
+
+  $('#metalnickeldeals-forward_price').on('change',function(){
+
+    commissions();
+  });
+
+  $('#metalnickeldeals-final_sales_price_per').on('change',function(){
+    commissions();
+  });
+
 });
 
-function costPrice(){
+function sums(){
   var pur_price = $('#metalnickeldeals-purchase_price_a').val();
   var ins_price = $('#metalnickeldeals-insurance_cost_a').val();
 
-  if (!$.isNumeric(pur_price)) {
-    pur_price = 0;
-  }
+//  if (pur_price != '' && ins_price != '') {
+    $.post("?r=metal-nickel-deals/ajax-sum",{
+          pur_price:pur_price,
+          ins_price:ins_price,
+      },
+      function(data,status){
+          $('#metalnickeldeals-total_cost_price').empty().val(data);
+      });
+//  }
 
-  if (!$.isNumeric(ins_price)) {
-    ins_price = 0;
-  }
-
-  var total = parseFloat(pur_price)+parseFloat(ins_price);
-  // /metalnickeldeals-total_cost_price
-  total = total.toFixed(2);
-  $('#metalnickeldeals-total_cost_price').val(total);
-//  var expiry_date = $('#expiry_date').val();
 }
 
-function costPrice_b(){
+
+function commissions(){
   var pur_price = $('#metalnickeldeals-purchase_price_a').val();
   var ins_price = $('#metalnickeldeals-insurance_cost_a').val();
+  var for_price = $('#metalnickeldeals-forward_price').val();
+  var final_percent = $('#metalnickeldeals-final_sales_price_per').val();
+//console.log('coms');
 
-  if (!$.isNumeric(pur_price)) {
-    pur_price = 0;
+  if (pur_price != '' && ins_price != '') {
+    $.post("?r=metal-nickel-deals/ajax-commission",{
+          pur_price:pur_price,
+          ins_price:ins_price,
+          for_price:for_price,
+          final_percent:final_percent,
+      },
+      function(data,status){
+        console.log(data);
+          var jsonObj = eval ("(" + data + ")");
+          $('#metalnickeldeals-unrealised_profit_a').empty().val(jsonObj.before_commission);
+          $('#metalnickeldeals-commision').empty().val(jsonObj.commission);
+          $('#metalnickeldeals-unrealised_profit_b').empty().val(jsonObj.after_commission);
+          $('#metalnickeldeals-net_unrealised').empty().val(jsonObj.net_realized);
+      });
   }
-
-  if (!$.isNumeric(ins_price)) {
-    ins_price = 0;
-  }
-
-  var total = parseFloat(pur_price)+parseFloat(ins_price);
-  total = total.toFixed(2);
-  return total;
-
-}
-
-
-function beforeCom(){
-  var final = $('#metalnickeldeals-final_sales_price').val();
-
-  //costPrice = Total Cost Prices B16
-  var costPrice = costPrice_b();//function call
-
-  var total = parseFloat(final) - parseFloat(costPrice);
-  total = total.toFixed(2);
-  $('#metalnickeldeals-unrealised_profit_a').val(total);
-
-  //comTotal = Commission b19
-  var com_total = commission(total);//function call
-
-  $('#metalnickeldeals-commision').val(com_total);
-  var after_com = total-com_total;
-  after_com = after_com.toFixed(2);
-  //after com = Realized Profit after Commission B20
-  $('#metalnickeldeals-unrealised_profit_b').val(after_com);
-
-  //return = Net Realised Percentage Returns
-  var return_per = (after_com/costPrice)*100;
-  return_per = return_per.toFixed(2);
-    console.log(return_per);
-  $('#metalnickeldeals-net_unrealised').val(return_per);
-
-}
-
-function commission(total){
-
-  var n_total = parseFloat(total)* parseFloat(0.15);
-  n_total = n_total.toFixed(2);
-//  $('#metalnickeldeals-commision').val(n_total);
-  return n_total;
-//  console.log(total);
 }
