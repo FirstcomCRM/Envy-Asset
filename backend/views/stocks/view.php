5319bb6c42dcaa94548a4f259daba081d1af9762
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-03-20 17:52:31 +0800

    added new fields to be displayed

--- backend/views/stocks/view.php
+++ backend/views/stocks/view.php
@@ -35,6 +35,12 @@ $this->params['breadcrumbs'][] = $this->title;
               'format' => ['date', 'php:d M Y'],
             ],
             [
+              'attribute'=>'sold_date',
+              'format' => ['date', 'php:d M Y'],
+            ],
+            'sold_units',
+            'buy_units',
+            [
               'attribute'=>'price',
               'value'=>function($model){
                 return '$'.Retrieve::retrieveFormat($model->price);
@@ -75,9 +81,35 @@ $this->params['breadcrumbs'][] = $this->title;
             //    return '$'.$model->unrealized;
               },
             ],
+            [
+              'attribute'=>'add_in_rate',
+              'value'=>function($model){
+                  return '$'.Retrieve::retrieveFormat($model->add_in_rate);
+            //    return '$'.$model->unrealized;
+              },
+            ],
+            [
+              'attribute'=>'buy_in_rate',
+              'value'=>function($model){
+                  return '$'.Retrieve::retrieveFormat($model->buy_in_rate);
+            //    return '$'.$model->unrealized;
+              },
+            ],
+            [
+              'attribute'=>'sold_price-rate',
+              'value'=>function($model){
+                  return '$'.Retrieve::retrieveFormat($model->sold_price_rate);
+            //    return '$'.$model->unrealized;
+              },
+            ],
+            [
+              'attribute'=>'month_end_rate',
+              'value'=>function($model){
+                  return '$'.Retrieve::retrieveFormat($model->unrealized_rate);
+            //    return '$'.$model->unrealized;
+              },
+            ],
 
-          //  'current_market:decimal',
-          //  'unrealized:decimal',
 
         ],
     ]) ?>
