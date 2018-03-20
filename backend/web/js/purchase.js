896c6faca9437881a3d1c8bdbba328115ec55576
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-03-20 17:55:01 +0800

    modified the main function name

--- backend/web/js/purchase.js
+++ backend/web/js/purchase.js
@@ -3,69 +3,38 @@ $(document).ready(function(){
   //Purchase Module Area
   //$("input[name=someRadioGroup]").on('change', function()
   $('input[name="Purchase[purchase_type]"]').on('change', function() {
-    sums();
+    pur_sum();
   });
 
   $('#price').on('change',function(){
-    sums();
+  //  datas();
+    pur_sum();
   });
 
   $('#purchase_date').on('change',function(){
   //  console.log('purchase date');
-    sums();
+    pur_sum();
   });
 
   $('#expiry_date').on('change',function(){
     //console.log('expire date');
-    sums();
+    pur_sum();
   });
 
   $('#charge_type').on('change',function(){
-    sums();
+    pur_sum();
   });
 
   $('#company_charge').on('change',function(){
-    sums();
+    pur_sum();
   });
 
   //End of Purchase Module Area
 
-  //Purchase Staff Area
-  $('input[name="PurchaseStaff[purchase_type]"]').on('change', function() {
-    s_sums();
-  });
-
-  $('#s_price').on('change',function(){
-    s_sums();
-  });
-
-  $('#s_purchase_date').on('change',function(){
-  //  console.log('purchase date');
-    s_sums();
-  });
-
-  $('#s_expiry_date').on('change',function(){
-    //console.log('expire date');
-    s_sums();
-  });
-
-  $('#s_charge_type').on('change',function(){
-    s_sums();
-  });
-
-  $('#s_company_charge').on('change',function(){
-    s_sums();
-  });
-
-  //end of Purchase Module Area
-
-
 
 });
 
-
-function sums(){
-
+function datas(){
   var price = $('#price').val();
   var purchase_date = $('#purchase_date').val();
   var expiry_date = $('#expiry_date').val();
@@ -73,9 +42,9 @@ function sums(){
   var charge_type = $('#charge_type').val();
   var company_charge = $('#company_charge').val();
   //company_charge
-  //console.log(price);
-//  console.log(purchase_type);
-//  console.log(charge_type);
+//  console.log(price);
+//  console.log(purchase_date);
+//  console.log(expiry_date);
   if (price != '' && purchase_date != '' && expiry_date != '') {
     $.post("?r=purchase/ajax-sum",{
           price:price,
@@ -102,22 +71,23 @@ function sums(){
   }else{
     console.log('error something');
   }
-
-
-
 }
 
-function s_sums(){
-  var price = $('#s_price').val();
-  var purchase_date = $('#s_purchase_date').val();
-  var expiry_date = $('#s_expiry_date').val();
-  var purchase_type = $('input[name="PurchaseStaff[purchase_type]"]:checked').val();
-  var charge_type = $('#s_charge_type').val();
-  var company_charge = $('#s_company_charge').val();
-  //company_charge
 
+function pur_sum(){
+
+  var price = $('#price').val();
+  var purchase_date = $('#purchase_date').val();
+  var expiry_date = $('#expiry_date').val();
+  var purchase_type = $('input[name="Purchase[purchase_type]"]:checked').val();
+  var charge_type = $('#charge_type').val();
+  var company_charge = $('#company_charge').val();
+  //company_charge
+  console.log(price);
+  console.log(purchase_type);
+  console.log(charge_type);
   if (price != '' && purchase_date != '' && expiry_date != '') {
-    $.post("?r=purchase-staff/ajax-sum",{
+    $.post("?r=purchase/ajax-sum",{
           price:price,
           purchase_date:purchase_date,
           expiry_date:expiry_date,
@@ -128,11 +98,14 @@ function s_sums(){
       },
       function(data, status){
         //  console.log(data);
-            $( "#s_sum_all" ).empty().val(data);
-
+          //    $( "#sum_all" ).empty().val(data);
       //    var content = $( data ).find( "#content" );
       //    $( "#result" ).empty().append(data);
 
+          var jsonObj = eval ("(" + data + ")");
+          $('#customer_earn').empty().val(jsonObj.customer_amount);
+          $('#company_earn').empty().val(jsonObj.company_earn);
+          $('#staff_earn').empty().val(jsonObj.staff_earn);
       //    $('.selected-item-list').append(data);
           // console.log(status);
       });
