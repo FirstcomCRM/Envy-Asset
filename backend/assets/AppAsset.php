30ebf2d25869e2b3fc5efa286c9bb5e8c2d0f1e0
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-05-03 16:12:39 +0800

    added stocks.js

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -23,6 +23,7 @@ class AppAsset extends AssetBundle
       'js/reports.js',
       'js/purchase.js',
       'js/nickel.js',
+      'js/stocks.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
