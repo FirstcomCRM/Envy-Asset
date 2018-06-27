d5ca8693089ec8984af3a87048780de7cdb14f5e
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-06-27 10:56:56 +0800

    added investor/staff list on dropdown

--- backend/views/withdraw/_form.php
+++ backend/views/withdraw/_form.php
@@ -4,17 +4,35 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use common\models\Investor;
+use common\models\UserManagement;
 use common\models\ProductManagement;
 use kartik\widgets\Select2;
 use kartik\widgets\DatePicker;
 /* @var $this yii\web\View */
 /* @var $model common\models\Withdraw */
 /* @var $form yii\widgets\ActiveForm */
-$data = Investor::find()->orderBy(['company_name'=>SORT_ASC])->select(['id','nric_comp'])->all();
-$invest = ArrayHelper::map($data,'id','nric_comp');
+
+if ($model->isNewRecord) {
+  $data = Investor::find()->orderBy(['company_name'=>SORT_ASC])->where(['id'=>0])->select(['id','nric_comp'])->all();
+  $invest = ArrayHelper::map($data,'id','nric_comp');
+}else{
+  if ($model->type == 'Investor') {
+    $data = Investor::find()->orderBy(['company_name'=>SORT_ASC])->select(['id','nric_comp'])->all();
+    $invest = ArrayHelper::map($data,'id','nric_comp');
+  }else{
+    $data = UserManagement::find()->orderBy(['name'=>SORT_ASC])->select(['id','name'])->all();
+    $invest = ArrayHelper::map($data,'id','name');
+  }
+}
+
 
 $data = ProductManagement::find()->orderBy(['product_name'=>SORT_ASC])->select(['id','product_name'])->all();
 $prod = ArrayHelper::map($data,'id','product_name');
+
+$type = [
+  'Investor'=>'Investor',
+  'Staff'=>'Staff',
+];
 
 $data = null;
 ?>
@@ -24,9 +42,19 @@ $data = null;
     <?php $form = ActiveForm::begin(); ?>
       <div class="row">
         <div class="col-md-6">
+          <?php echo $form->field($model,'type')->widget(Select2::className(),[
+             'data'=>$type,
+             'options'=>['placeholder'=>'Select Type ', 'id'=>'withdraw-type'],
+             'theme'=> Select2::THEME_BOOTSTRAP,
+             'size'=> Select2::MEDIUM,
+             'pluginOptions' => [
+               'allowClear' => true
+             ],
+           ]) ?>
+
           <?php echo $form->field($model,'investor')->widget(Select2::className(),[
              'data'=>$invest,
-             'options'=>['placeholder'=>'Select... '],
+             'options'=>['placeholder'=>'Select... ', 'id'=>'withdraw-investor'],
              'theme'=> Select2::THEME_BOOTSTRAP,
              'size'=> Select2::MEDIUM,
              'pluginOptions' => [
@@ -43,7 +71,18 @@ $data = null;
              ],
            ]) ?>
 
-          <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'price')->widget(\yii\widgets\MaskedInput::className(), [
+             'options' => ['id' => 'price','class'=>'form-control'],
+             'clientOptions' => [
+               'alias' => 'decimal',
+               'digits' => 2,
+               'digitsOptional' => false,
+               'radixPoint' => '.',
+               'groupSeparator' => ',',
+               'autoGroup' => true,
+               'removeMaskOnSubmit' => true,
+             ],
+           ]) ?>
 
           <?php $form->field($model, 'date')->textInput() ?>
           <?php echo $form->field($model, 'date')->widget(DatePicker::classname(), [
