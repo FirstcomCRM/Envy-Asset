48262a24de03173b98321901f3d94203b9e0dfa2
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-06-27 10:52:11 +0800

    added investor/staff on view list

--- backend/views/purchase/view.php
+++ backend/views/purchase/view.php
@@ -16,10 +16,16 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $metal = [
   'purchase_no',
+  'type',
   [
     'attribute'=>'investor',
     'value'=>function($model){
-      return Retrieve::retrieveInvestor($model->investor);
+      if ($model->type == 'Investor') {
+        return Retrieve::retrieveInvestor($model->investor);
+      }else{
+        return Retrieve::retrieveUsernameManagement($model->investor);
+      }
+
     },
   ],
   [
