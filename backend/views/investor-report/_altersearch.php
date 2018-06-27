cf9d851879ca201c7ce88a39914b585ef4b7ba5c
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-06-27 10:43:41 +0800

    modifed investor dropdown list and added email button

--- backend/views/investor-report/_altersearch.php
+++ backend/views/investor-report/_altersearch.php
@@ -3,8 +3,10 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
+use kartik\widgets\Spinner;
 //use kartik\daterange\DateRangePicker;
 use yii\jui\DatePicker;
+use kartik\widgets\Select2;
 use common\models\Investor;
 /* @var $this yii\web\View */
 /* @var $model common\models\PurchaseSearch */
@@ -12,13 +14,18 @@ use common\models\Investor;
 
 $data = Investor::find()->select(['id','company_name'])->orderBy(['company_name'=>SORT_ASC])->all();
 $investors = ArrayHelper::map($data,'id','company_name');
-
+$test = Spinner::widget(['preset' => 'large', 'align' => 'center']);
 ?>
 <style>
 .ui-datepicker table{
     display: none;
 }
+
 </style>
+
+<div class="overlay">
+    <div id="loading-img"></div>
+</div>
 
 <div class="alter-search">
 
@@ -41,14 +48,23 @@ $investors = ArrayHelper::map($data,'id','company_name');
     </div>
 
     <div class="col-md-3">
-      <?php echo $form->field($model,'id')->label(false)->dropDownList($investors,['prompt'=>'Select Investor']) ?>
+      <?php echo $form->field($model,'id')->label(false)->widget(Select2::className(),[
+        'data'=>$investors,
+        'options'=>['placeholder'=>'Investor '],
+        'theme'=> Select2::THEME_BOOTSTRAP,
+        'size'=> Select2::MEDIUM,
+        'pluginOptions' => [
+          'allowClear' => true
+        ],
+      ]) ?>
     </div>
 
 
-    <div class="col-md-3">
+    <div class="col-md-4">
       <?= Html::submitButton('<i class="fa fa-search" aria-hidden="true"></i> Search', ['class' => 'btn btn-default']) ?>
-
       <?php echo Html::a('<i class="fa fa-undo" aria-hidden="true"></i> Reset',['alter'],['class' => 'btn btn-default']) ?>
+      <?php echo Html::a('<i class="fa fa-print" aria-hidden="true"></i> Email Button A',['alter-email'],['class' => 'btn btn-default','id'=>'multi-email']) ?>
+
 
     </div>
   </div>
