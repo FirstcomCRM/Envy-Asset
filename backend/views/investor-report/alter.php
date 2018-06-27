a14461fca5bffbaf33058f661c80bff5e9a73f4d
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-06-27 10:46:36 +0800

    added new list on table and added a spinner widget

--- backend/views/investor-report/alter.php
+++ backend/views/investor-report/alter.php
@@ -6,8 +6,9 @@ use yii\widgets\Pjax;
 use yii\bootstrap\Modal;
 use yii\helpers\Url;
 use common\components\Retrieve;
+use kartik\widgets\Spinner;
 
-$this->title = 'Investor Report Alt';
+$this->title = 'Investor Report';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
@@ -19,6 +20,13 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
     <div class="panel-body">
         <?php  echo $this->render('_altersearch', ['model' => $searchModel]); ?>
+
+        <div class="well" id="loaders" style="display:none">
+          <?php echo Spinner::widget(['preset' => 'large', 'align' => 'center', 'caption' => 'Loading &hellip;']); ?>
+          <div class="clearfix"></div>
+        </div>
+
+
         <!---
         <div id="email-div">
         <?php Html::checkbox('confirm', false, ['label' => 'Confirm Report' ,'class'=>'email-checkbox']); ?>
@@ -41,24 +49,37 @@ $this->params['breadcrumbs'][] = $this->title;
         <?php Html::a('<i class="fa fa-wheelchair" aria-hidden="true"></i> Modal Testing Update', FALSE, ['value' => Url::to(['investor-report/update', 'id' => 3]), 'class' => 'btn btn-default modalButton','id'=>'modalButton' ]); ?>
       </p>
 
-        <?php Pjax::begin(['id'=>'investor', 'linkSelector'=>false]); ?>
+
          <?= GridView::widget([
             'dataProvider' => $dataProvider,
+            'id' => 'griditems',
           //  'filterModel' => $searchModel,
             'columns' => [
                 ['class' => 'yii\grid\SerialColumn'],
 
+              [
+                'class' => 'yii\grid\CheckboxColumn',
+
+                'checkboxOptions'=>function($model){
+                  return ['value'=>$model->id];
+                }
+              ],
               'company_name',
               'nric',
+              'email',
               [
                 'header'=>'Action',
                 'class'=>'yii\grid\ActionColumn',
               //   'template'=>'{view}{update}{download-pdf}{email}',
-                'template'=>'{download-pdf}',
+                'template'=>'{download-pdf} {print}',
                 'options'=>['style'=>'padding:5px'],
                 'buttons'=>[
                   'download-pdf'=>function($url,$model,$key){
                           return Html::a('<i class="fa fa-file-pdf-o" aria-hidden="true"></i>', ['alter-download','id'=>$key], ['title'=>'Download PDF', 'target'=>'_blank']);
+                    //  return Html::a('<i class="fa fa-file-pdf-o" aria-hidden="true"></i>', $url, ['title'=>'Download PDF' ,'id'=>'test']);
+                  },
+                  'print'=>function($url,$model,$key){
+                          return Html::a('<i class="fa fa-envelope" aria-hidden="true"></i>', ['alter-print','id'=>$key], ['title'=>'Email','class'=>'emails']);
                     //  return Html::a('<i class="fa fa-file-pdf-o" aria-hidden="true"></i>', $url, ['title'=>'Download PDF' ,'id'=>'test']);
                   },
                 ],
@@ -67,7 +88,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             ],
         ]); ?>
-      <?php Pjax::end(); ?>
+
     </div>
   </div>
 </div>
